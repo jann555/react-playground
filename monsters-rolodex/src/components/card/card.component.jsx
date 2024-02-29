05c317e5e8055f2d1d1377188b180442fd5a6e3b
Jannick Ocampo <focampo3@gatech.edu>
2024-02-29 16:33:59 -0500

    Finished Rolodex

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -1,19 +1,16 @@
-import { Component } from "react";
-
 import './card.styles.css';
 
+const Card = ({monster}) => {
+    const {name, id, email} = monster;
 
-class Card extends Component {
-    render(){
-        const { name, id, email } = this.props.monster;
-        return (
-            <div className='card-container' key={id}>
-                <img alt={`monster ${name}}`} src={`https://robohash.org/${id}?set=set7&size=180x180`}/>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        )
-    }
-}
+    return (
+    <div className='card-container' key={id}>
+        <img alt={`monster ${name}}`} src={`https://robohash.org/${id}?set=set7&size=180x180`}/>
+        <h2>{name}</h2>
+        <p>{email}</p>
+    </div>
+    );
+    
+};
 
 export default Card;
