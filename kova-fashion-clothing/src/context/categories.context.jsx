18a41a6e0d6bc9f1beb2c4d3cf0ba8f66181accd
Jannick Ocampo <focampo3@gatech.edu>
2024-03-12 20:33:04 -0400

    Added products from firebase

---
+++ kova-fashion-clothing/src/context/categories.context.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+//import SHOP_DATA from "../routes/shop/shop-data";
+
+export const CategoriesContext = createContext({
+    categoriesMap: [],
+})
+
+export const CategoriesProvider = ({children}) =>{
+    const [categoriesMap, setcategoriesMap] = useState({});
+
+    useEffect(() =>{
+        const getCategoriesMap = async () =>{
+            const categoryMap = await getCategoriesAndDocuments();
+            setcategoriesMap(categoryMap);
+        }
+        getCategoriesMap();
+        
+    }, [])
+
+    console.log(categoriesMap)
+
+    const value = {categoriesMap};
+
+    return (<CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>);
+}
